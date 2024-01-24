4189a17b286efff6f1fc923332cdf96f35fc451f
QuangVinh1601 <140512444+QuangVinh1601@users.noreply.github.com>
2024-01-24 22:59:35 +0700

    Modified Program.cs again

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Quang Vinh");
+            Console.WriteLine("Hello World! Quang Vinh! Xin chào");
         }
     }
 }
